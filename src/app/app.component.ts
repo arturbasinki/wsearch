d84dd72520ad98bbbac9e3ad9caf5ee637989ffb
airbass <artur.basinski@gmail.com>
2022-01-22 01:09:07 +0100

    Ready functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search; //this follow from API response structure
+      // console.log(response);
+    });
+  }
 }
+
+// https://en.wikipedia.org/w/api.php?
+// action=query&
+// format=json&
+// list=search&
+// utf8=1&
+// srsearch=space
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
 
-  constructor() { }
+  term = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+    // console.log(this.term);
   }
 
+  // that below works with first <input/> in template:
+  // onInput(value: any) {
+  //   this.term = value.target.value;
+  //   console.log(this.term);
+  // }
 }
